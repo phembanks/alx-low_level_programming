d094ff39b9cad3fbebf7dde5290794b565dc7176
phembanks <adesujofemi@gmail.com>
2022-09-10 03:35:27 -0700

    4. Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  * main - print the string in the put function
  * 
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle"
- * Return 0
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
  int main(void)
  {
