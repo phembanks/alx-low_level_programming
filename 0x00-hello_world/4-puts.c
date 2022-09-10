a444ffa7dee592b62c60a1cc8da94a745dcd3693
phembanks <adesujofemi@gmail.com>
2022-09-10 03:29:09 -0700

    4. Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -9,6 +10,6 @@
  */
  int main(void)
  {
- 	puts("\" Programming is like building a multilingual puzzle");
+	puts("\" Programming is like building a multilingual puzzle");
 	return (0);
 }
