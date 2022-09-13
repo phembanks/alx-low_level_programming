2788fe9b6d9f3fc71d6a028c9a4336b08fdde1f7
phembanks <adesujofemi@gmail.com>
2022-09-12 20:54:26 -0700

    task 2 commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,24 +12,20 @@
  *
  * * Return: 0
  *
- * */
+ *
+ */
 
 int main(void)
 
 {
-
 	char ch;
-
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-
 	{
 
 			putchar(ch);
 
 	}
-
 	putchar('\n');
-
 	return (0);
 
 }
