88e688827b11c70aacb6b4491607804a893b5b66
phembanks <adesujofemi@gmail.com>
2022-09-12 17:58:07 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlb.h>
+#include <time.h>
+/**
+ * main - Betty Style
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
+	return (0);
+}
