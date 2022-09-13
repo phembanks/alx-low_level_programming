5369cff12f288afba2f17228ab22020083e1bff7
phembanks <adesujofemi@gmail.com>
2022-09-12 21:28:18 -0700

    2 task if

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,9 @@
 /**
  *  main - print if the number is postive, zero, or negative
  *
- * 
  *  Description: using the main function
- *
  *  this program prints "Programming is positive, zero, or negative
- *
  *  Return: 0
- *
- *
  */
 
 int main(void)
