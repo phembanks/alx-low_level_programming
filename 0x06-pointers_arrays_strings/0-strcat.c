dc7dad5370873837f86d078351b611a1d6232c32
phembanks <adesujofemi@gmail.com>
2022-09-22 01:20:03 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#include "main.h"
+
 /**
- * void prototypes()
- * int prototypes()
- * char prototypes()
+ * _strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
  */
-void reverse_array(int *a, int n);
-void print_number(int n);
-void print_buffer(char *b, int size);
-int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif /*MAIN_H*/
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
 
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
