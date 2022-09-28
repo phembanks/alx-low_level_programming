3ba47c70052fc7e9bd90a9904337cca31ad0906b
phembanks <adesujofemi@gmail.com>
2022-09-28 00:19:50 -0700

    0-mem

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 /**
- * void prototypes ()
- * int prototypes ()
+ * void prototypes()
+ * int prototypes()
  */
 
 int _putchar(char);
@@ -14,5 +14,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int siz;
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif /*MAIN_H*/
